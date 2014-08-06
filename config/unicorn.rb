b3acb4473acbe5abc016f093b54372cb9d0b0246
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-08-06 01:15:14 -0300

    add unicorn conf file

---
+++ config/unicorn.rb
@@ -0,0 +1,11 @@
+worker_processes 2
+timeout 25
+preload_app true
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+end
