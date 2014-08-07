cac8b3d2e14c3adc4c484abe81ccc2ce5d7cb495
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-08-07 06:41:29 -0300

    page to sample images transformations

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -2,7 +2,14 @@ class AlbumController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
 
+  respond_to :html
+
   def show
+  end
+
+  def photo
+    @photo = current_user.photos.find(params[:id])
+    respond_with @photo
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# config/routes.rb
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
+  get '/photos/:id' => 'album#photo', as: :albums_photo
   root to: 'album#show'
   mount Attachinary::Engine => '/attachinary'
 end
